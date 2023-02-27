934eb42f6479b9bf31e4575aacb28480ccc33667
Didier Irias <didier.irias@gmail.com>
2023-02-26 19:41:05 -0600

    Calibrating the model and making prediction for 2019

--- Aplicaciones de AI/models_evaluations.py
+++ Aplicaciones de AI/models_evaluations.py
@@ -1,42 +1,90 @@
-from sklearn.linear_model import LinearRegression
-from statsmodels.tsa.holtwinters import ExponentialSmoothing
-from pmdarima.arima import auto_arima
-from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error
 import pandas as pd
+import numpy as np
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error
+import matplotlib.pyplot as plt
 
-# cargar los datos históricos y de prueba
-historico = pd.read_csv("datos_historicos.csv")
-prueba = pd.read_csv("datos_prueba.csv")
+# Cargar los datos
+data = pd.read_excel('EJEMPLO.xlsx', parse_dates=['FECHA'], index_col='FECHA')
 
-# modelo de regresión múltiple
-regresion = LinearRegression()
-regresion.fit(historico[["variable1", "variable2", "variable3"]], historico["objetivo"])
-prediccion_regresion = regresion.predict(prueba[["variable1", "variable2", "variable3"]])
-mape_regresion = mean_absolute_percentage_error(prueba["objetivo"], prediccion_regresion)
-mse_regresion = mean_squared_error(prueba["objetivo"], prediccion_regresion)
+# Dividir los datos en conjuntos de entrenamiento y prueba
+train = data.loc['2015':'2017']
+test = data.loc['2015']
+# Modelo de regresión múltiple
+X_train = np.array([np.arange(len(train)), train.index.month]).T
+y_train = train['Facturacion'].values.reshape(-1, 1)
+reg = LinearRegression().fit(X_train, y_train)
 
-# modelo de Holt-Winters
-holt_winters = ExponentialSmoothing(historico["objetivo"], seasonal_periods=12, trend="add", seasonal="add")
-holt_winters.fit()
-prediccion_holt_winters = holt_winters.forecast(len(prueba))
-mape_holt_winters = mean_absolute_percentage_error(prueba["objetivo"], prediccion_holt_winters)
-mse_holt_winters = mean_squared_error(prueba["objetivo"], prediccion_holt_winters)
+# Predecir los valores de prueba
+X_test = np.array([np.arange(len(test)), test.index.month]).T
+y_pred_reg = reg.predict(X_test)
 
-# modelo SARIMA
-sarima = auto_arima(historico["objetivo"], seasonal=True, m=12, trace=True)
-prediccion_sarima = sarima.predict(n_periods=len(prueba))
-mape_sarima = mean_absolute_percentage_error(prueba["objetivo"], prediccion_sarima)
-mse_sarima = mean_squared_error(prueba["objetivo"], prediccion_sarima)
+# Calcular el MAPE y MSE para el modelo de regresión múltiple
+mape_reg = mean_absolute_percentage_error(test['Facturacion'], y_pred_reg)
+mse_reg = mean_squared_error(test['Facturacion'], y_pred_reg)
+#import pdb; pdb.set_trace()
 
-# imprimir los resultados
-print("MAPE regresión múltiple:", mape_regresion)
-print("MSE regresión múltiple:", mse_regresion)
-print("MAPE Holt-Winters:", mape_holt_winters)
-print("MSE Holt-Winters:", mse_holt_winters)
-print("MAPE SARIMA:", mape_sarima)
-print("MSE SARIMA:", mse_sarima)
-#En este ejemplo, se ajustan tres modelos diferentes y se calculan el MAPE y MSE para cada uno de ellos. Los resultados se imprimen en la consola. El modelo que tenga el MAPE y MSE más bajo será el mejor modelo según estas métricas. Sin embargo, es importante tener en cuenta que otras métricas y factores también pueden ser importantes al seleccionar el mejor modelo para un conjunto de datos específico.
+# Modelo de Holt Winters
+hw_model = ExponentialSmoothing(train['Facturacion'], seasonal_periods=12, trend='add', seasonal='add').fit()
 
+# Predecir los valores de prueba
+y_pred_hw = hw_model.predict(start=test.index[0], end=test.index[-1])
 
+# Calcular el MAPE y MSE para el modelo de Holt Winters
+mape_hw = mean_absolute_percentage_error(test['Facturacion'], y_pred_hw)
+mse_hw = mean_squared_error(test['Facturacion'], y_pred_hw)
 
+# Modelo de SARIMA
+sarima_model = SARIMAX(train['Facturacion'], order=(1, 1, 1), seasonal_order=(1, 1, 1, 12)).fit()
 
+# Predecir los valores de prueba
+y_pred_sarima = sarima_model.predict(start=test.index[0], end=test.index[-1])
+
+# Calcular el MAPE y MSE para el modelo de SARIMA
+mape_sarima = mean_absolute_percentage_error(test['Facturacion'], y_pred_sarima)
+mse_sarima = mean_squared_error(test['Facturacion'], y_pred_sarima)
+
+# Imprimir los resultados
+print('MAPE Regresión Múltiple:', mape_reg)
+print('MSE Regresión Múltiple:', mse_reg)
+print('MAPE Holt Winters:', mape_hw)
+print('MSE Holt Winters:', mse_hw)
+print('MAPE SARIMA:', mape_sarima)
+print('MSE SARIMA:', mse_sarima)
+
+# Testing
+
+# Datos
+modelos = ['Regresión Múltiple', 'Holt Winters', 'SARIMA']
+mape_scores = [mape_reg, mape_hw, mape_sarima]
+mse_scores = [mape_reg, mse_hw, mse_sarima]
+
+# Gráfico de barras para MAPE
+plt.figure(figsize=(8, 6))
+plt.bar(modelos, mape_scores, color='blue')
+plt.title('MAPE Scores')
+plt.xlabel('Modelos')
+plt.ylabel('MAPE')
+plt.ylim([0, 30])
+plt.show()
+
+# Gráfico de barras para MSE
+plt.figure(figsize=(8, 6))
+plt.bar(modelos, mse_scores, color='red')
+plt.title('MSE Scores')
+plt.xlabel('Modelos')
+plt.ylabel('MSE')
+plt.ylim([0, 300000000])
+plt.show()
+
+# Making projection for 2019# make forecast for 12 months (2019)
+
+# Predecir los valores de 2019
+start = pd.to_datetime('2019-01-01')
+end = pd.to_datetime('2019-12-01')
+y_pred_hw = hw_model.predict(start=start, end=end)
+
+# Imprimir los valores proyectados para 2019
+print(y_pred_hw)
